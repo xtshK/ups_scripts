fbf231732d4306b01e43654573c9b7646f0aa746
Selena <selens0207@gmail.com>
2025-06-17 11:49:41 +0800

    feat: Add run_ups.bat script for executing UPS data processing in a virtual environment

---
+++ run_ups.bat
@@ -0,0 +1,17 @@
+@echo off
+echo processing...
+
+REM 切換到目前 bat 所在資料夾
+cd /d "%~dp0"
+
+REM 啟用虛擬環境
+call .venv\Scripts\activate.bat
+
+REM 執行主程式
+python main_v2.py
+
+REM 停用虛擬環境（可選）
+call .venv\Scripts\deactivate.bat
+
+echo Done
+pause
